8d9430476ea8b5c7e8c89a132a82ede96d5f4586
Hugh Luscombe <hughluscombe@MacBook-Pro.local>
2019-05-09 13:39:04 +1000

    Updates

---
+++ precache-manifest.bee7053635e19423bce486dcb940c196.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95d48a72c505c38eb952b798ccf734e2",
+    "url": "/hluscombe/burning-airlines-client/index.html"
+  },
+  {
+    "revision": "8f51a1d257f9c8aa3aaf",
+    "url": "/hluscombe/burning-airlines-client/static/css/main.337f9cb5.chunk.css"
+  },
+  {
+    "revision": "7276bb1e91d230125bdc",
+    "url": "/hluscombe/burning-airlines-client/static/js/2.0cb37d3b.chunk.js"
+  },
+  {
+    "revision": "8f51a1d257f9c8aa3aaf",
+    "url": "/hluscombe/burning-airlines-client/static/js/main.ce1a564f.chunk.js"
+  },
+  {
+    "revision": "55f01aa14077aa370011",
+    "url": "/hluscombe/burning-airlines-client/static/js/runtime~main.7f97d02e.js"
+  }
+]);
